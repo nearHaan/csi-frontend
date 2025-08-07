0678513b3e02857767ea909c9c10eb3a7019fd81
nearHaan <farhaan.nizam675@gmail.com>
2025-08-07 16:11:20 +0530

    Initial Commit

---
+++ src/routes/team/+page.svelte
@@ -0,0 +1,22 @@
+<div>
+    <div class="w-full h-40 flex items-center justify-center border-y-2 border-black">
+        <h1 class="text-2xl">MEET OUR TEAM</h1>
+    </div>
+    <div class="flex">
+        <div class="m-3">
+            <div class="h-40 w-40 bg-black"></div>
+            <h2 class="mt-2 text-white text-sm font-bold">Raihan Rasheed</h2>
+            <h3 class="mt-1 text-[#999999] text-sm">Chair person</h3>
+        </div>
+        <div class="m-3">
+            <div class="h-40 w-40 bg-black"></div>
+            <h2 class="mt-2 text-white text-sm font-bold">Raihan Rasheed</h2>
+            <h3 class="mt-1 text-[#999999] text-sm">Chair person</h3>
+        </div>
+        <div class="m-3">
+            <div class="h-40 w-40 bg-black"></div>
+            <h2 class="mt-2 text-white text-sm font-bold">Raihan Rasheed</h2>
+            <h3 class="mt-1 text-[#999999] text-sm">Chair person</h3>
+        </div>
+    </div>
+</div>
