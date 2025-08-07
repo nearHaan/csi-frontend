a8ef681a51f02473bd6861f9fe9e10b3b7d7c066
nearHaan <farhaan.nizam675@gmail.com>
2025-08-08 01:54:40 +0530

    UI Update

--- src/routes/team/+page.svelte
+++ src/routes/team/+page.svelte
@@ -1,9 +1,11 @@
-<div class="w-full max-w-6xl">
-	<div class="flex h-40 w-full items-center justify-center border-b border-x-1 border-black">
+<div class="flex w-full flex-col items-center">
+	<div
+		class="flex h-40 w-full max-w-6xl items-center justify-center border-x-1 border-b border-black"
+	>
 		<h1 class="text-4xl">MEET OUR TEAM</h1>
 	</div>
 	<div
-		class="mx-auto grid grid-cols-2 place-items-center justify-center gap-2 p-5 sm:grid-cols-3 md:grid-cols-4 border-black border-x-1"
+		class="mx-auto grid w-full max-w-6xl grid-cols-2 place-items-center justify-center gap-2 border-x-1 border-black p-5 sm:grid-cols-3 md:grid-cols-4"
 	>
 		<div class="m-3">
 			<div class="h-40 w-40 bg-black"></div>
@@ -26,11 +28,13 @@
 			<h3 class="mt-1 text-sm text-[#999999]">Chair person</h3>
 		</div>
 	</div>
-	<div class="flex h-20 w-full items-center justify-start border-y-1 border-black">
-		<h2 class="flex h-full items-center justify-center bg-black px-10 text-2xl">TECH TEAM</h2>
+	<div class="flex w-full justify-center border-y-1 border-black">
+		<div class="flex h-20 w-full max-w-6xl items-center justify-start">
+			<h2 class="flex h-full items-center justify-center bg-black px-10 text-2xl">TECH TEAM</h2>
+		</div>
 	</div>
 	<div
-		class="mx-auto grid grid-cols-2 place-items-center justify-center gap-2 p-5 sm:grid-cols-3 md:grid-cols-4 border-black border-x-1"
+		class="mx-auto grid w-full max-w-6xl grid-cols-2 place-items-center justify-center gap-2 border-x-1 border-black p-5 sm:grid-cols-3 md:grid-cols-4"
 	>
 		<div class="m-3">
 			<div class="h-40 w-40 bg-black"></div>
@@ -53,7 +57,9 @@
 			<h3 class="mt-1 text-sm text-[#999999]">Chair person</h3>
 		</div>
 	</div>
-	<div class="flex h-20 w-full items-center justify-start border-y-1 border-black">
-		<h2 class="flex h-full items-center justify-center bg-black px-10 text-2xl">TECH TEAM</h2>
+	<div class="flex w-full justify-center border-y-1 border-black">
+		<div class="flex h-20 w-full max-w-6xl items-center justify-start">
+			<h2 class="flex h-full items-center justify-center bg-black px-10 text-2xl">TECH TEAM</h2>
+		</div>
 	</div>
 </div>
