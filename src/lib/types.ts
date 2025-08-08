c3281f40a241784b213149d16f59219fb141a186
nearHaan <farhaan.nizam675@gmail.com>
2025-08-08 20:44:39 +0530

    Cleanup code

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -8,3 +8,36 @@ export type Event = {
     fees: number;
     image: string;
 }
+
+export type ExecomMember = {
+    id: number;
+    name: string;
+    position: string;
+    social_link: string;
+    profile_link: string;
+}
+
+export type ExecomList = Record<string, ExecomMember[]>;
+
+// let execomList = {
+//     "techTeam": [
+//         {
+//             name: "Ram",
+//             pos: "Chair"
+//         },
+//         {
+//             name: "Joy",
+//             pos: "VC"
+//         },
+//     ],
+//     "design-team": [
+//         {
+//             name: "Tim",
+//             pos: "Chair"
+//         },
+//         {
+//             name: "Joy",
+//             pos: "VC"
+//         },
+//     ],
+// };
