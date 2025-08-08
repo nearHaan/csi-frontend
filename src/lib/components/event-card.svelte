a2c7bd9e7454917162fbc0d95c67f9d8bd96d4c2
nearHaan <farhaan.nizam675@gmail.com>
2025-08-08 23:37:15 +0530

    Added Event Compnent

---
+++ src/lib/components/event-card.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+    import type { Event } from "$lib/types";
+
+    const {
+        event,
+    } : {
+        event: Event,
+    } = $props();
+</script>
+
+<div class="z-0 overflow-hidden bg-white shadow-[4px_4px_0_0_black]">
+	<div class="h-48 overflow-hidden bg-black">
+		<img alt="event-poster" src={event.image} />
+	</div>
+	<div class="flex flex-col items-center p-4">
+		<div class="w-full">
+			<h3 class="text-lg font-bold text-gray-800">{event.title}</h3>
+			<p class="mb-2 text-sm text-gray-400">{event.subtitle}</p>
+			<p class="text-sm text-gray-600">{event.location}</p>
+		</div>
+		<button
+			class="mt-2 ml-2 cursor-pointer bg-[#BFBFBF] p-2 text-black hover:bg-black hover:text-white"
+			>Register</button
+		>
+	</div>
+	<div class="flex w-full items-center justify-between bg-[#BFBFBF] p-2 text-black">
+		<p class="font-bold">{event.date}</p>
+		<p>{event.time}</p>
+	</div>
+</div>
