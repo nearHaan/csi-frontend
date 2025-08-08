b3618862b9a227b5f11a35e98e3e027eb1d933d8
nearHaan <farhaan.nizam675@gmail.com>
2025-08-08 23:38:35 +0530

    Updated conditions

--- src/lib/components/event-card.svelte
+++ src/lib/components/event-card.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
-    import type { Event } from "$lib/types";
+	import type { Event } from '$lib/types';
 
-    const {
-        event,
-    } : {
-        event: Event,
-    } = $props();
+	const {
+		event,
+		isUpcoming = false
+	}: {
+		event: Event;
+		isUpcoming?: boolean;
+	} = $props();
 </script>
 
 <div class="z-0 overflow-hidden bg-white shadow-[4px_4px_0_0_black]">
@@ -18,10 +20,12 @@
 			<p class="mb-2 text-sm text-gray-400">{event.subtitle}</p>
 			<p class="text-sm text-gray-600">{event.location}</p>
 		</div>
-		<button
-			class="mt-2 ml-2 cursor-pointer bg-[#BFBFBF] p-2 text-black hover:bg-black hover:text-white"
-			>Register</button
-		>
+		{#if isUpcoming}
+			<button
+				class="mt-2 ml-2 cursor-pointer bg-[#BFBFBF] p-2 text-black hover:bg-black hover:text-white"
+				>Register</button
+			>
+		{/if}
 	</div>
 	<div class="flex w-full items-center justify-between bg-[#BFBFBF] p-2 text-black">
 		<p class="font-bold">{event.date}</p>
