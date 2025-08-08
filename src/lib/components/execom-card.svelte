eb7baa6d2fc849846658bea9a32e3112fbdae8ff
nearHaan <farhaan.nizam675@gmail.com>
2025-08-08 20:44:58 +0530

    Added Execom Card

---
+++ src/lib/components/execom-card.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	let {
+        id,
+        name = "",
+        pos = "",
+        social_link = "",
+        profile_img = "",
+    } : {
+        id: number,
+        name: string,
+        pos: string,
+        social_link: string,
+        profile_img: string
+    } = $props();
+</script>
+
+<div class="m-3">
+	<div class="h-40 w-40 bg-black">
+        <img alt="profile-img" src={profile_img}/>
+    </div>
+	<h2 class="mt-2 text-sm font-bold text-white">{name}</h2>
+	<h3 class="mt-1 text-sm text-[#999999]">{pos}</h3>
+</div>
