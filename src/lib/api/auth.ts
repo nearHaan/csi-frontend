a43a3ab6f9b8af5cd0e3bcf7f3793459196929b4
nearHaan <farhaan.nizam675@gmail.com>
2025-08-12 09:42:51 +0530

    Registration API

--- src/lib/api/auth.ts
+++ src/lib/api/auth.ts
@@ -1,5 +1,21 @@
 import { API_BASE_URL } from "$env/static/private";
 
+export async function registerUser(name: string, email: string, batch: string, year: number, phone_number: string, password: string, confirm_password: string) {
+    const res = await fetch(`${API_BASE_URL}/api/auth/register/`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({name, email, batch, year, phone_number, password, confirm_password})
+    });
+
+    if (!res.ok) {
+        const error = await res.json().catch(() => ({}));
+        console.log(error);
+        throw new Error(error.meesage || 'Registration failed');
+    }
+
+    return await res.json();
+}
+
 export async function loginUser(email: string, password: string) {
     const res = await fetch(`${API_BASE_URL}/api/auth/login/`, {
         method: 'POST',
