c48615a9501a1ee67049178b98d31ba1b3572386
nearHaan <farhaan.nizam675@gmail.com>
2025-08-14 21:25:17 +0530

    UI Update

--- src/routes/admin/dashboard/+page.svelte
+++ src/routes/admin/dashboard/+page.svelte
@@ -1,6 +1,6 @@
 <div class="flex min-h-screen flex-col items-center bg-[#222222] text-white">
 	<h1 class="my-10 font-mono text-4xl">Admin Dashboard</h1>
-	<div class="flex w-full max-w-4xl gap-2 p-2 max-sm:flex-col">
+	<div class="w-full max-w-4xl gap-2 p-2 min-sm:grid grid-cols-2 max-sm:flex flex-col">
 		<div class="my-2 flex flex-col border-1 border-black p-4 min-sm:w-full">
 			<h2>People</h2>
 			<div
@@ -33,6 +33,19 @@
 				</div>
 				<div class="flex items-center p-2">
 					<a href="/admin/dashboard">Manage Registration</a>
+				</div>
+			</div>
+		</div>
+		<div class="my-2 flex flex-col border-1 border-black p-4 min-sm:w-full">
+			<h2>Admin Settings</h2>
+			<div
+				class="mt-2 flex flex-col [&>*:nth-child(even)]:bg-[#767676] [&>*:nth-child(odd)]:bg-[#313131]"
+			>
+				<div class="flex items-center p-2">
+					<a href="/admin/dashboard">Manage Admins</a>
+				</div>
+				<div class="flex items-center p-2">
+					<a href="/admin/dashboard">Change Password</a>
 				</div>
 			</div>
 		</div>
