32027efe03d60d05cfb272f03eb3d874ac3a060c
nearhaan <141166709+nearHaan@users.noreply.github.com>
2025-08-26 21:53:56 +0530

    Update fetch path

--- src/lib/server/api/events.ts
+++ src/lib/server/api/events.ts
@@ -2,7 +2,7 @@ import { PUBLIC_API_URL } from '$env/static/public';
 import type { EventList } from '$lib/types';
 
 export async function getEventsData(): Promise<EventList> {
-	const res = await fetch(`${PUBLIC_API_URL}/api/events/`, {
+	const res = await fetch(`/api/events/`, {
 		method: 'GET',
 		headers: { 'Content-Type': 'application/json' }
 	});
